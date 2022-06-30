093a6c92cda05da36307b42d2eebb56b20d3fc9d
Alejandro Cruz <hcruz0716@gmail.com>
2022-06-29 23:56:55 -0600

    Add loading and counterdown

--- components/home.vue
+++ components/home.vue
@@ -1,31 +1,65 @@
 <template>
-  <div class="h-screen w-screen bg-img">
-    <div class="pt-96">
-      <h1 class="text-xl text-center pt-36">Sofía Solis</h1>
-    </div>
-    <div>
+  <div class="h-screen w-screen bg-img flex justify-center">
+    <div class="z-10 bottom-5 absolute p-4">
+      <h1 class="text-xl text-center">Sofía</h1>
       <h1 class="text-xl text-center">Mis XV Años</h1>
-    </div>
-    <div id="clock">
-      <div class="flex justify-around text-center">
-        <span>122</span>
-        <span>12</span>
-        <span>1</span>
-        <span>45</span>
-      </div>
-      <div class="flex justify-around text-center">
-        <span>Días</span>
-        <span>Horas</span>
-        <span>Minutos</span>
-        <span>Segundos</span>
+      <h1 v-if="message === true">¡YA EMPEZÓ!</h1>
+      <div v-else class="grid-cols-4 flex">
+        <div class="px-2">
+          <h1>{{ remainDays }}</h1>
+          <p>Dias</p>
+        </div>
+        <div class="px-2">
+          <h1>{{ remainHours }}</h1>
+          <p>Horas</p>
+        </div>
+        <div class="px-2">
+          <h1>{{ remainMinutes }}</h1>
+          <p>Minutos</p>
+        </div>
+        <div class="px-2">
+          <h1>{{ remainSeconds }}</h1>
+          <p>Segundos</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      remainSeconds: 0,
+      remainMinutes: 0,
+      remainHours: 0,
+      remainDays: 0,
+      message: false,
+    };
+  },
+  mounted() {
+    this.getDate("Dec 3 2022 20:00:00 GMT-0600");
+  },
+  methods: {
+    getDate(deadline) {
+      let now = new Date();
+      this.remainTime = (new Date(deadline) - now + 1000) / 1000;
+      this.remainSeconds = ("0" + Math.floor(this.remainTime % 60)).slice(-2);
+      this.remainMinutes = ("0" + Math.floor((this.remainTime / 60) % 60)).slice(-2);
+      this.remainHours = ("0" + Math.floor((this.remainTime / 3600) % 24)).slice(-2);
+      this.remainDays = Math.floor(this.remainTime / (3600 * 24));
+
+      const timerUpdate = setInterval(() => {
+        this.getDate(deadline);
+
+        if (this.remainTime <= 1) {
+          clearInterval(timerUpdate);
+          this.message = true;
+        }
+      }, 1000);
+    },
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./assets/**/*.{css}",
     "./components/*.{vue,js}",
     "./components/**/*.{vue,js}",
@@ -12,7 +11,46 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   variants: {
     extend: {},
