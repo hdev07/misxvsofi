8fe6ccafe1b955407613a53b67ff0cc66a3927a4
Alejandro Cruz <hcruz0716@gmail.com>
2022-07-04 19:53:50 -0600

    update gitignore file

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
     <Home />
-    <!-- <Frase /> -->
+    <Frase />
     <Lugar />
   </v-app>
 </template>
 <script>
 import Home from "./components/home.vue";
-// import Frase from "./components/frase.vue";
+import Frase from "./components/frase.vue";
 import Lugar from "./components/lugar.vue";
 
 export default {
